e95874af99e8e7fc76ce532934dd360772c56e39
NarayanRungta <141025229+NarayanRungta@users.noreply.github.com>
2025-05-06 17:01:06 +0530

    updated event page

--- src/components/subComponents/EventCard.jsx
+++ src/components/subComponents/EventCard.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 
 function EventCard({ imgSource, eventId, eventName }) {
   return (
-    <div className="w-[24rem] border-2 border-textColor1 rounded-md h-[24rem] p-4 flex flex-col gap-8">
+    <div className="w-[24rem] border-2 border-textColor1 rounded-md h-[24rem] p-4 transform hover:scale-105 transition-all ease-in-out flex flex-col gap-8">
       <div className="overflow-hidden w-88 border-neutral-300 h-72 rounded-md border-2">
         <figure>
           <img src={imgSource} alt={`Event ${eventId}`} />
