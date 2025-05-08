eff0041001424f6c85bfb5ba96c8b72f6b113b1a
VanXodus305 <soumyadeep.30505@gmail.com>
2025-05-08 23:31:34 +0530

    fix: add line-clamp to feedback text in FeedbackCard component for better readability

--- src/components/subComponents/FeedbackCard.jsx
+++ src/components/subComponents/FeedbackCard.jsx
@@ -8,7 +8,7 @@ function FeedbackCard({
 }) {
   return (
     <div className="flex flex-col items-center justify-between p-4 gap-4 rounded-br-3xl border-2 border-textColor1 ">
-      <p className="text-textColor2 dark:text-black text-[18px] w-full text-left">{feedback}</p>
+      <p className="text-textColor2 dark:text-black text-[18px] w-full text-left line-clamp-5">{feedback}</p>
       <div className="w-full flex items-center justify-end gap-2">
         <figure className="rounded-full dark:text-black">
           {/* <img className="rounded-full aspect-square object-cover" src={imgSource} alt="" width={24} height={24} /> */}
