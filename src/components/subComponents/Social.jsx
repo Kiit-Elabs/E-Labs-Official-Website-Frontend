3b1edcb9a9d75460438a4c98b4b14c7caeb7af71
VanXodus305 <soumyadeep.30505@gmail.com>
2025-05-11 18:00:33 +0530

    fix: reorder LinkedIn section in Social component for consistency

--- src/components/subComponents/Social.jsx
+++ src/components/subComponents/Social.jsx
@@ -6,16 +6,6 @@ function Social() {
       <h1 className="font-extrabold text-xl text-textColor1">Follow Us On</h1>
       <div className="flex flex-col gap-1 items-center sm:items-start">
         <div className="flex flex-col text-textColor2 dark:text-black text-sm gap-2">
-          <div className="flex text-textColor2 dark:text-black text-sm gap-2 justify-start items-center m-0 p-0">
-            <img src="../Images/linkedin (1).png" alt="" width={18} />
-            <a
-              target="_blank"
-              href="https://www.linkedin.com/company/kiit-elabs"
-              className="hover:text-textColor1 text-textColor2 dark:text-black duration-200 focus:text-textColor1 "
-            >
-              LinkedIn
-            </a>
-          </div>
           <div className="flex gap-2 justify-start items-center">
             <img src="../Images/instagram (2).png" alt="" width={18} />
             <a
@@ -24,6 +14,16 @@ function Social() {
               className="hover:text-textColor1 text-textColor2 dark:text-black duration-200 focus:text-textColor1"
             >
               Instagram
+            </a>
+          </div>
+          <div className="flex text-textColor2 dark:text-black text-sm gap-2 justify-start items-center m-0 p-0">
+            <img src="../Images/linkedin (1).png" alt="" width={18} />
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/company/kiit-elabs"
+              className="hover:text-textColor1 text-textColor2 dark:text-black duration-200 focus:text-textColor1 "
+            >
+              LinkedIn
             </a>
           </div>
           <div className="flex gap-2 justify-start items-center">
